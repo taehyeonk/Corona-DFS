02563edb7779bf005edbe3da83c153d80c1bbe3f
DarkScene <xogus9641@naver.com>
2020-07-24 17:24:06 +0900

    menu update & map upload test

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# 전국 확진자 수에 대한 시각화 지도
+@app.route('/kor_defCnt_map')
+def kor_defCnt_map():
+    return render_template('kor_defCnt_map.html')
+
+# 전국 사망자 수에 대한 시각화 지도
+@app.route('/kor_deathCnt_map')
+def kor_deathCnt_map():
+    return render_template('kor_deathCnt_map.html')
+
+# 전국 격리 해제 인원에 대한 시각화 지도
+@app.route('/kor_isolClearCnt_map')
+def kor_isolClearCnt_map():
+    return render_template('kor_isolClearCnt_map.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=8089)
